7eae97330f0cfe7c80b8652c2ca5f38b96046d6f
RaghuVasant <raghuvasant@gmail.com>
2016-11-16 23:38:05 -0600

    Add files via upload
    Refactored for code smells (added mappings for refactored classes)

--- app/femr/util/dependencyinjection/modules/MapperModule.java
+++ app/femr/util/dependencyinjection/modules/MapperModule.java
@@ -20,8 +20,7 @@ package femr.util.dependencyinjection.modules;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.name.Names;
-import femr.common.IItemModelMapper;
-import femr.common.ItemModelMapper;
+import femr.common.*;
 import femr.data.DataModelMapper;
 import femr.data.IDataModelMapper;
 
@@ -32,5 +31,9 @@ public class MapperModule extends AbstractModule {
 
         bind(IDataModelMapper.class).to(DataModelMapper.class);
         bind(IItemModelMapper.class).annotatedWith(Names.named("identified")).to(ItemModelMapper.class);
+        bind(IIPatientItemModelMapper.class).to(PatientItemModelMapper.class);
+        bind(IPrescriptionItemModelMapper.class).to(PrescriptionItemModelMapper.class);
+
+
     }
 }
